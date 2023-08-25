3b95ba890345e7b54d8ad3dcc7e645dc754d70be
otinu <fitshter@gmail.com>
2023-08-25 17:17:45 +0900

    2ファイル間で連携するよう修正しました

--- gmail_api.py
+++ gmail_api.py
@@ -3,30 +3,28 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 # 独自ラベルのIDを別ファイルで管理
-import my_label
+import my_confidential
 
-SCOPES = "https://www.googleapis.com/auth/gmail.modify"
-store = file.Storage("storage.json")
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
-    creds = tools.run_flow(flow, store)
-GMAIL = discovery.build("gmail", "v1", http=creds.authorize(Http()))
 
-user_id = "me"
-label_id_one = "INBOX"
-label_id_two = my_label.MyLabel.name
-
-unread_msgs = (
-    GMAIL.users()
-    .messages()
-    .list(userId=user_id, labelIds=[label_id_one, label_id_two])
-    .execute()
-)
-
-mssg_list = unread_msgs["messages"]
-m_id = mssg_list[0]["id"]  # 対象ラベルの最新メールだけ取得
-message = GMAIL.users().messages().get(userId=user_id, id=m_id).execute()
-mail_body = message["snippet"]
-
-print(mail_body[6:12])
+def get_authentication_code():
+    SCOPES = "https://www.googleapis.com/auth/gmail.modify"
+    store = file.Storage("storage.json")
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
+        creds = tools.run_flow(flow, store)
+    GMAIL = discovery.build("gmail", "v1", http=creds.authorize(Http()))
+    user_id = "me"
+    label_id_one = "INBOX"
+    label_id_two = my_confidential.Information.label
+    unread_msgs = (
+        GMAIL.users()
+        .messages()
+        .list(userId=user_id, labelIds=[label_id_one, label_id_two])
+        .execute()
+    )
+    mssg_list = unread_msgs["messages"]
+    m_id = mssg_list[0]["id"]  # 対象ラベルの最新メールだけ取得
+    message = GMAIL.users().messages().get(userId=user_id, id=m_id).execute()
+    mail_body = message["snippet"]
+    print(mail_body[6:12])
--- test.py
+++ test.py
@@ -1,6 +1,20 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver = webdriver.Chrome()  # 引数なしで動いた
+# ローカルファイル読み込み
+import my_confidential
+import gmail_api
+
+# VSCodeから実行する際、ディレクトリを変更
+os.chdir(f"{my_confidential.Information.directory}")
+
+# 認証メール取得
+gmail_api.get_authentication_code()
+
+# Webドライバー起動
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://ai-inter1.com/python-selenium/")
 time.sleep(3)
