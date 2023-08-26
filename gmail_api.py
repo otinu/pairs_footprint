90d5c3af298ba0750639c54c811e79ba11685650
otinu <fitshter@gmail.com>
2023-08-26 19:53:43 +0900

    説明文追加,変数名修正をしました

--- gmail_api.py
+++ gmail_api.py
@@ -6,7 +6,17 @@ from oauth2client import file, client, tools
 import my_confidential
 
 
-def get_authentication_code():
+def get_authentication_code() -> str:
+    """
+    ログイン時の認証メールから認証コードを取得する
+
+    Args:
+        None
+
+    Returns:
+        str: 認証コード
+    """
+
     SCOPES = "https://www.googleapis.com/auth/gmail.modify"
     store = file.Storage("storage.json")
     creds = store.get()
@@ -16,15 +26,16 @@ def get_authentication_code():
     GMAIL = discovery.build("gmail", "v1", http=creds.authorize(Http()))
     user_id = "me"
     label_id_one = "INBOX"
-    label_id_two = my_confidential.Information.label
-    unread_msgs = (
+    label_id_two = my_confidential.Information.LABEL
+    mylabel_msgs = (
         GMAIL.users()
         .messages()
         .list(userId=user_id, labelIds=[label_id_one, label_id_two])
         .execute()
     )
-    mssg_list = unread_msgs["messages"]
+    mssg_list = mylabel_msgs["messages"]
     m_id = mssg_list[0]["id"]  # 対象ラベルの最新メールだけ取得
     message = GMAIL.users().messages().get(userId=user_id, id=m_id).execute()
     mail_body = message["snippet"]
+
     print(mail_body[6:12])
